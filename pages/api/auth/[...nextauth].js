26bb547b1debfca1392a7b1abc2a913366df18af
kenrazalan <kennethrazalan29@gmail.com>
2021-06-22 00:48:46 +0800

    Login

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    Providers.Facebook({
+        clientId: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET
+      }),
+    // ...add more providers here
+  ],
+
+  // A database is optional, but required to persist accounts in a database
+  database: process.env.DATABASE_URL,
+})
