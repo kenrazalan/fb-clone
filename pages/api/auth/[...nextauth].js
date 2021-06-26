3de2021ff1a48c33264a4da643caab00ee914c58
kenrazalan <kennethrazalan29@gmail.com>
2021-06-26 20:12:37 +0800

    remove guthub and fb auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,14 +4,14 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
-    Providers.Facebook({
-        clientId: process.env.FACEBOOK_ID,
-        clientSecret: process.env.FACEBOOK_SECRET
-      }),
+    // Providers.GitHub({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET
+    // }),
+    // Providers.Facebook({
+    //     clientId: process.env.FACEBOOK_ID,
+    //     clientSecret: process.env.FACEBOOK_SECRET
+    //   }),
       Providers.Google({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET
